cffd438b6a7b104e97a62a1c9dfff91058bf17fd
Denis Rysavy <72613152+rysavy122@users.noreply.github.com>
2023-01-09 15:46:25 +0100

    Update schemas.js

--- joi/schemas.js
+++ joi/schemas.js
@@ -1,8 +1,7 @@
 import Joi from 'joi';
 
 export const userSchema = Joi.object({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
+  name: Joi.string().required(),
   email: Joi.string().required(),
   password: Joi.string().alphanum().min(8).max(12).required()
 });
